fe24d1c6cd395c620bef29f4e7951a80c697283a
ninaffb04 <ninaffb2004@gmail.com>
2025-04-25 14:56:22 -0600

    database created

--- ProjectSourceCode/src/init_data/create.sql
+++ ProjectSourceCode/src/init_data/create.sql
@@ -1,1 +1,25 @@
+DROP TABLE IF EXISTS users CASCADE;
+CREATE TABLE IF NOT EXISTS users (
+  user_id SERIAL PRIMARY KEY NOT NULL,
+  username VARCHAR(50) NOT NULL,
+  password VARCHAR(225) NOT NULL,
+  email VARCHAR(50) NOT NULL,
+  last_login DATE,
+  streak INT DEFAULT 0,
+  name VARCHAR(100),
+  friends_count INT,
+  avatar_url VARCHAR(200)
+);
+
+DROP TABLE IF EXISTS friendships CASCADE;
+CREATE TABLE IF NOT EXISTS friendships (
+  id SERIAL PRIMARY KEY NOT NULL,
+  user_id INT NOT NULL,
+  friend_id INT NOT NULL,
+  status ENUM('pending', 'accepted', 'blocked') NOT NULL DEFAULT 'pending',
+  requested_at DATETIME NOT NULL,
+  updated_at DATETIME NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES users (user_id) ON DELETE CASCADE,
+  FOREIGN KEY (friend_id) REFERENCES users (user_id) ON DELETE CASCADE
+);
 
